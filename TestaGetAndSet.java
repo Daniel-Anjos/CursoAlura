61976d3c23b78d8b7d270135999164d79286f1e0
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2018-06-02 20:02:16 -0300

    Update TesteGetAndSet
    Novos comentários

--- TestaGetAndSet.java
+++ TestaGetAndSet.java
@@ -1,10 +1,20 @@
 public class TestaGetAndSet {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
+		Conta conta = new Conta(325,987654);
 		System.out.println(conta.getNumero());
 		conta.setNumero(4321);
 		System.out.println(conta.getNumero());
 		conta.setAgencia(987);
 		System.out.println(conta.getAgencia());
+		
+		Cliente michele = new Cliente();
+		michele.setNome("Michele Theobald");
+		//adicionando a referencia do cliente na classe conta
+		conta.setTitular(michele);
+		//Para imprimir, é necessário informar as referências
+		System.out.println(conta.getTitular().getNome());
+		//Para alterar também
+		conta.getTitular().setProfissao("Engenheira");
+		
 	}
 }
