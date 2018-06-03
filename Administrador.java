3b0326f9bacf2efb7bf09c5351d066a939c0fe71
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2018-06-03 18:13:54 -0300

    Create Administrador.java
    Exemplificando a utilização de herança e interface em uma mesma classe

---
+++ Administrador.java
@@ -0,0 +1,26 @@
+//Aqui herdamos da classe funcionario e implementamos a interface autenticavel
+public class Administrador extends Funcionario implements Autenticavel{
+
+	private int senha;
+	
+	@Override
+	public double getBonificacao() {
+		return 150;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
